6c64d4bd42e602bd635a3d043abc1e4fa4f2c614
Christopher Groskopf <staringmonkey@gmail.com>
2015-11-05 09:25:16 -0600

    Expand tests. Closes #11. #12.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,16 +1,16 @@
-0.3.0
------
+0.3.0 - November 5, 2015
+------------------------
 
 * Removed Python 2.6 support.
 * Updated agate dependency to version 1.1.0.
 * Additional SQL types are now supported. (#4, #10)
 
-0.2.0
------
+0.2.0 - October 22, 2015
+------------------------
 
 * Add explicit patch function.
 
-0.1.0
------
+0.1.0 - September 22, 2015
+--------------------------
 
 * Initial version.
--- tests/test_agatesql.py
+++ tests/test_agatesql.py
@@ -15,16 +15,19 @@ agatesql.patch()
 class TestSQL(unittest.TestCase):
     def setUp(self):
         self.rows = (
-            (1, 'a'),
-            (2, 'b'),
-            (None, None)
+            (1, 'a', True, '11/4/2015', '11/4/2015 12:22 PM'),
+            (2, u'👍', False, '11/5/2015', '11/4/2015 12:45 PM'),
+            (None, 'b', None, None, None)
         )
 
-        self.number_type = agate.Number()
-        self.text_type = agate.Text()
+        self.column_names = [
+            'number', 'text', 'boolean', 'date', 'datetime'
+        ]
 
-        self.column_names = ('one', 'two')
-        self.column_types = (self.number_type, self.text_type)
+        self.column_types = [
+            agate.Number(), agate.Text(), agate.Boolean(),
+            agate.Date(), agate.DateTime()
+        ]
 
         self.table = agate.Table(self.rows, self.column_names, self.column_types)
         self.connection_string = 'sqlite:///:memory:'
@@ -40,6 +43,9 @@ class TestSQL(unittest.TestCase):
         self.assertSequenceEqual(table.column_names, self.column_names)
         self.assertIsInstance(table.column_types[0], agate.Number)
         self.assertIsInstance(table.column_types[1], agate.Text)
+        self.assertIsInstance(table.column_types[2], agate.Boolean)
+        self.assertIsInstance(table.column_types[3], agate.Date)
+        self.assertIsInstance(table.column_types[4], agate.DateTime)
 
         self.assertEqual(len(table.rows), len(self.table.rows))
         self.assertSequenceEqual(table.rows[0], self.table.rows[0])
